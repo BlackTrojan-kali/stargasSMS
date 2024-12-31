b8ad6b6dcfb74bb3a002da9cbd30468e2a03be22
BlackTrojan <chendjousil@gmail.com>
2024-12-31 16:08:24 +0800

    udate on sales list

--- resources/views/commercial/historique-ventes.blade.php
+++ resources/views/commercial/historique-ventes.blade.php
@@ -5,32 +5,64 @@
         <h1 class="p-2 mt-5 font-bold text-2xl">Historique des {{ strtoupper($type) }}S
         </h1>
         <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
-        <table class="history scroll mt-10 w-full border-2 border-collapse border-gray-400 ">
+        <table id="table-ventes" class="history scroll mt-10 w-full border-2 border-collapse border-gray-400 ">
             <thead class="p-3 bg-gray-500 text-white">
-                <td>
-                    nom Client
-                </td>
-                <td>
-                    adresse
-                </td>
-                <td>
-                    prix total
-                </td>
-                <td>
-                    date
-                </td>
-                <td>actions</td>
+                <tr>
+                    <th>
+                        nom Client
+                    </th>
+                    <th colspan="6">
+                        qtes vendus
+                    </th>
+                    <th>
+                        prix total
+                    </th>
+                    <th>
+                        date
+                    </th>
+                    <th>actions</td>
+                </tr>
+                <tr class="border  border-black">
+                    <th class="border  border-black">/</th>
+                    <th class="border  border-black" colspan="2">50 KG</th>
+                    <th class="border  border-black" colspan="2">12.5 KG</th>
+                    <th class="border  border-black" colspan="2">6 KG</th>
+                    <th class="border  border-black">/</th>
+                    <th class="border  border-black">/</th>
+                    <th class="border  border-black">/</th>
+                </tr>
+                <tr class="border  border-black">
+                    <th class="border  border-black">/</th>
+                    <th class="border  border-black">Prix U</th>
+                    <th class="border  border-black">Qte</th>
+                    <th class="border  border-black">Prix U</th>
+                    <th class="border  border-black">Qte</th>
+                    <th class="border  border-black">Prix U</th>
+                    <th class="border  border-black">Qte</th>
+                    <th class="border  border-black">/</th>
+                    <th class="border  border-black">/</th>
+                    <th class="border  border-black">/</th>
+                </tr>
             </thead>
             <tbody>
+            
                 @foreach ($ventes as $vente)
                     <tr id="{{ $vente->id }}" class="hover:text-white hover:bg-blue-400 hover:cursor-pointer">
-                        <td>{{ $vente->customer }}</td>
-                        <td>{{ $vente->address }}</td>
-                        <td>{{ $vente->prix_6 * $vente->qty_6 + $vente->prix_12 * $vente->qty_12 + $vente->prix_50 * $vente->qty_50 }}
+                        <td class="border  border-black">{{ $vente->customer }}</td>
+                        <td class="border  border-black">{{ $vente->prix_50 }}</td>
+                        <td class="border  border-black">{{ $vente->qty_50 }}</td>
+                        <td class="border  border-black">{{ $vente->prix_12 }}</td>
+                        <td class="border  border-black">{{ $vente->qty_12 }}</td>
+                        <td class="border  border-black">{{ $vente->prix_6 }}</td>
+                        <td class="border  border-black">{{ $vente->qty_6 }}</td>
+                        <td class="border  border-black">
+                            {{ $vente->prix_6 * $vente->qty_6 + $vente->prix_12 * $vente->qty_12 + $vente->prix_50 * $vente->qty_50 }}
                         </td>
-                        <td>{{ $vente->created_at }}</td>
-                        <td><a href="{{ route('printInvoice', ['id' => $vente->id]) }}"><i class="text-teal-900">print</i></a> <i
-                                class="text-red-500 delete">delete</i></td>
+                        <td class="border  border-black">{{ $vente->created_at }}</td>
+                        <td class="border  border-black"><a href="{{ route('printInvoice', ['id' => $vente->id]) }}"><i
+                                    class="text-teal-900">print</i></a>
+                            <i class="text-red-500 delete">delete</i>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
@@ -38,7 +70,7 @@
     </div>
     <script type="module">
         $(function() {
-            $(".delete").on("click", function() {
+            $("#table-ventes").on("click", ".delete", function() {
                 var id = $(this).parent().parent().attr("id");
                 var token = $("meta[name='csrf-token']").attr("content");
                 Swal.fire({
